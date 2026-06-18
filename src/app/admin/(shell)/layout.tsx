69ad31d2840863c64f213ee126235693d4b0e85a
Alisa Nosova <alisa@nosova.pro>
2026-06-18 01:35:34 -0600

    auto-deploy: 01:35:34

--- src/app/admin/(shell)/layout.tsx
+++ src/app/admin/(shell)/layout.tsx
@@ -1,5 +1,10 @@
 import AdminShellLayout from './admin-layout';
+import { AdminLangProvider } from '@/lib/adminLang';
 
 export default function AdminShellRouteLayout({ children }: { children: React.ReactNode }) {
-  return <AdminShellLayout>{children}</AdminShellLayout>;
+  return (
+    <AdminLangProvider>
+      <AdminShellLayout>{children}</AdminShellLayout>
+    </AdminLangProvider>
+  );
 }
